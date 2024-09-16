fcef4b9bde5dd55ad4849110b6836a27ea31e093
Juliana Nogueira Peixoto <juliananpeixoto01@ufrrj.br>
2024-09-15 21:18:44 -0300

    Realizando o front da página inicial

---
+++ src/View/scripts.js
@@ -0,0 +1,10 @@
+document.getElementById("criarButton").onclick = function() {
+    const modal = new bootstrap.Modal(document.getElementById('modalCriarTarefa'));
+    modal.show(modal);
+};
+document.getElementById("visualizarButton").onclick = function() {
+    alert('Você clicou no botão');
+};
+document.getElementById("editarButton").onclick = function() {
+    alert('Você clicou no botão');
+};
