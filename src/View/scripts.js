76ac23ff74136eebad2431f2fd90f698b257f105
Juliana Nogueira Peixoto <juliananpeixoto01@ufrrj.br>
2024-09-17 20:01:17 -0300

    Front end para 'criar tarefa' concluído

--- src/View/scripts.js
+++ src/View/scripts.js
@@ -1,10 +1,13 @@
-document.getElementById("criarButton").onclick = function() {
+document.getElementById("criarButton").onclick = function () {
     const modal = new bootstrap.Modal(document.getElementById('modalCriarTarefa'));
     modal.show(modal);
 };
-document.getElementById("visualizarButton").onclick = function() {
+document.getElementById("visualizarButton").onclick = function () {
     alert('Você clicou no botão');
 };
-document.getElementById("editarButton").onclick = function() {
+document.getElementById("editarButton").onclick = function () {
     alert('Você clicou no botão');
 };
+
+
+
